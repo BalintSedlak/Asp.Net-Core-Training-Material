18d4d9212658fa80a598120d068b76f52635818b
Balint Sedlak <sedlak.balint@gmail.com>
2023-07-28 08:49:49 +0200

    Add compose and mssql server

---
+++ 5_Docker/Restaurant.Docker.WebApp/Endpoints/GetTestEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurant.Docker.WebApp.Endpoints;
+
+public class GetTestEndpoint : EndpointBaseAsync
+    .WithoutRequest
+    .WithActionResult<string>
+{
+    [HttpGet("/Test")]
+    public override async Task<ActionResult<string>> HandleAsync(CancellationToken cancellationToken = default)
+    {
+        return "Backend is working";
+    }
+}
--- 5_Docker/Restaurant.Docker.WebApp/Program.cs
+++ 5_Docker/Restaurant.Docker.WebApp/Program.cs
@@ -16,8 +16,15 @@ builder.Services.AddSwaggerGen(c =>
     c.UseApiEndpoints();
 });
 
+string connectionString = CreateConnectionString(builder);
+
+if (builder.Environment.IsDevelopment())
+{
+    connectionString = builder.Configuration.GetConnectionString("ConnectionString");
+}
+
 // Add services to the container.
-builder.Services.AddDbContext<RestaurantContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")));
+builder.Services.AddDbContext<RestaurantContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddScoped<IAsyncRepository<CategoryEntity>, EfRepository<CategoryEntity>>();
 builder.Services.AddScoped<IAsyncRepository<CustomerDemographicEntity>, EfRepository<CustomerDemographicEntity>>();
@@ -41,6 +48,8 @@ builder.Services.AddScoped<ProductService>();
 
 var app = builder.Build();
 
+MigrationInitialisation(app);
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -58,9 +67,32 @@ app.UseEndpoints(endpoints =>
     endpoints.MapControllers();
 });
 
+
 app.Run();
 
 
+
+
+static string CreateConnectionString(WebApplicationBuilder builder)
+{
+    var server = builder.Configuration["DatabaseServer"];
+    var port = builder.Configuration["DatabasePort"];
+    var user = builder.Configuration["DatabaseUser"];
+    var password = builder.Configuration["DatabasePassword"];
+    var databaseName = builder.Configuration["DatabaseName"];
+
+    var connectionString = $"Server={server},{port};Initial Catalog={databaseName};User ID={user};Password={password};TrustServerCertificate=true";
+    
+    return connectionString;
+}
+
+static void MigrationInitialisation(IApplicationBuilder application)
+{
+    using (var serviceScope = application.ApplicationServices.CreateScope())
+    {
+        serviceScope.ServiceProvider.GetService<RestaurantContext>().Database.Migrate();
+    }
+}
 
 // Make the implicit Program class public so test projects can access it
 public partial class Program { }
