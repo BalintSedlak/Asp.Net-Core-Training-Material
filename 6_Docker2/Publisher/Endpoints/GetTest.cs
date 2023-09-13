197da981e38022c117d9153a2e2c84d6aa8f3b1a
Balint Sedlak <sedlak.balint@gmail.com>
2023-09-13 20:53:02 +0200

    update projects

---
+++ 6_Docker2/Publisher/Endpoints/GetTest.cs
@@ -0,0 +1,18 @@
+﻿using Ardalis.ApiEndpoints;
+using Mapster;
+using Microsoft.AspNetCore.Mvc;
+using SharedKernel.Messaging;
+using System.Text.Json;
+
+namespace Publisher.Endpoints;
+
+public class GetTest : EndpointBaseAsync
+    .WithoutRequest
+    .WithActionResult
+{
+    [HttpGet("/Test")]
+    public override async Task<ActionResult> HandleAsync(CancellationToken cancellationToken = default)
+    {
+        return Ok("Test");
+    }
+}
---
+++ 6_Docker2/Subscriber/Endpoints/GetTest.cs
@@ -0,0 +1,15 @@
+﻿using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Subscriber.Endpoints;
+
+public class GetTest : EndpointBaseAsync
+    .WithoutRequest
+    .WithActionResult
+{
+    [HttpGet("/Test")]
+    public override async Task<ActionResult> HandleAsync(CancellationToken cancellationToken = default)
+    {
+        return Ok("Test");
+    }
+}
